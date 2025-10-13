858d7625a4dd1e66fa9193cb07b37689dc6376c9
Soybean <soybeanjs@outlook.com>
2025-10-13 16:35:20 +0800

    refactor(projects): use new elegant-router plugin

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -1,40 +1,39 @@
-import type { CustomRoute, ElegantConstRoute, ElegantRoute } from '@elegant-router/types';
-import { generatedRoutes } from '../elegant/routes';
-import { layouts, views } from '../elegant/imports';
-import { transformElegantRoutesToVueRoutes } from '../elegant/transform';
-
-/**
- * custom routes
- *
- * @link https://github.com/soybeanjs/elegant-router?tab=readme-ov-file#custom-route
- */
-const customRoutes: CustomRoute[] = [];
+import type { AutoRouterRedirect, AutoRouterRoute } from '@elegant-router/types';
+import { routes } from '../_generated/routes';
+import { layouts, views } from '../_generated/imports';
+import { transformToVueRoutes } from '../_generated/transformer';
 
 /** create routes when the auth route mode is static */
 export function createStaticRoutes() {
-  const constantRoutes: ElegantRoute[] = [];
+  const constantRoutes: AutoRouterRoute[] = [];
+  const authRoutes: AutoRouterRoute[] = [];
 
-  const authRoutes: ElegantRoute[] = [];
+  let rootRoute: AutoRouterRedirect | undefined;
 
-  [...customRoutes, ...generatedRoutes].forEach(item => {
+  routes.forEach(item => {
     if (item.meta?.constant) {
       constantRoutes.push(item);
     } else {
       authRoutes.push(item);
     }
+
+    if (item.name === 'Root') {
+      rootRoute = item as AutoRouterRedirect;
+    }
   });
 
   return {
     constantRoutes,
-    authRoutes
+    authRoutes,
+    rootRoute: rootRoute as AutoRouterRedirect
   };
 }
 
 /**
  * Get auth vue routes
  *
- * @param routes Elegant routes
+ * @param authRoutes Elegant routes
  */
-export function getAuthVueRoutes(routes: ElegantConstRoute[]) {
-  return transformElegantRoutesToVueRoutes(routes, layouts, views);
+export function getAuthVueRoutes(authRoutes: AutoRouterRoute[]) {
+  return transformToVueRoutes(authRoutes, layouts, views);
 }
